71a45dec111069df55224f9478f21eb0c290d0be
KinKam Zhao <Kamzh88@KinKams-Air.fios-router.home>
2019-11-28 14:58:12 -0500

    html, routes, data

---
+++ app/data/friendsData.js
@@ -0,0 +1,51 @@
+var friendsArray = [
+    {
+        name: "Ahmed",
+        photo: "www.facebook.com/Ahmed",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "John",
+        photo: "www.facebook.com/John",
+        scores: [
+            8,
+            3,
+            7,
+            5,
+            4,
+            8,
+            10,
+            2,
+            1,
+            5
+        ]
+    },
+    {
+        name: "Bob",
+        photo: "www.facebook.com/Bob",
+        scores: [
+            4,
+            9,
+            2,
+            1,
+            5,
+            7,
+            9,
+            1,
+            3,
+            7
+        ]
+    }
+];
+module.exports = friendsArray;
---
+++ server.js
@@ -0,0 +1,21 @@
+// Series of npm packages that we will use to give our server useful functionality
+var express = require("express");
+var path = require("path");
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
