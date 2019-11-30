3df5330bf5dc63bbccc6554f270a98754c697601
KinKam Zhao <Kamzh88@KinKams-Air.fios-router.home>
2019-11-29 20:32:20 -0500

    modal working

--- app/data/friendsData.js
+++ app/data/friendsData.js
@@ -4,47 +4,47 @@ var friendsArray = [
         photo: "www.facebook.com/Ahmed",
         scores: [
             5,
-            1,
-            4,
-            4,
             5,
-            1,
-            2,
             5,
-            4,
-            1
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
         ]
     },
     {
         name: "John",
         photo: "www.facebook.com/John",
         scores: [
-            8,
             3,
-            7,
-            5,
-            4,
-            8,
-            10,
-            2,
-            1,
-            5
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
         ]
     },
     {
         name: "Bob",
         photo: "www.facebook.com/Bob",
         scores: [
-            4,
-            9,
-            2,
             1,
-            5,
-            7,
-            9,
             1,
-            3,
-            7
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
         ]
     }
 ];
