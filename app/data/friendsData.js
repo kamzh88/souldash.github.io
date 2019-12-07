61e3e13beb68b64e306a694df6ee160bf1e42b25
KinKam Zhao <Kamzh88@KinKams-MacBook-Air.local>
2019-12-07 12:59:14 -0500

    add new data to friends list

--- app/data/friendsData.js
+++ app/data/friendsData.js
@@ -1,7 +1,7 @@
 var friendsArray = [
     {
         name: "Joe",
-        photo: "www.facebook.com/Joe",
+        photo: "https://www.google.com/search?q=image+of+a+man&sxsrf=ACYBGNRKSFV9wwEm9WDOqf3ahPsMIqgN1w:1575732596696&tbm=isch&source=iu&ictx=1&fir=0ZUIIstGDHEfgM%253A%252CsiXBgr-E-CQ1BM%252C_&vet=1&usg=AI4_-kQI3oICR0OZ2azwjRBzy9MbnEHJCw&sa=X&ved=2ahUKEwj0mb-t7aPmAhXrmOAKHaC0Di0Q9QEwAHoECAgQKw#imgrc=0ZUIIstGDHEfgM:",
         scores: [
             5,
             5,
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,8 +6,45 @@ module.exports = function(app) {
         res.json(friendsData);
     })
 
-    // app.get("/api/friends/:friend", function (req, res) {
-    //     var chosen = req.params.friend;
-    //     console.log(chosen);
-    // })
+    app.post("/api/friends", function(req,res) {
+        console.log(req.body)
+        var friend = compare(req.body, friendsData )
+        console.log(friendsData)
+        friendsData.push(req.body);
+        res.json({friend:friend})
+    })
+
+    function compare(newSurvey, friendsData) {
+                var userValue = [];
+                for (var i = 0; i < newSurvey.scores.length; i++) {
+                    userValue.push(`${parseInt(newSurvey.scores[i])}`);          
+                };
+        
+                var score = [];
+                var name = [];
+                var totalDiff = [];
+                for (var i = 0; i < friendsData.length; i++) {
+                    var count = 0;
+                    for (var j = 0; j < friendsData[i].scores.length; j++) {
+                        // console.log("friends score" + friendsData[i].scores[j]);
+                        var x = Math.abs(userValue[j] - friendsData[i].scores[j])
+                        count += x
+                        // console.log(x);
+                    }
+                    // console.log(count);
+                    totalDiff.push(count);
+                }
+                console.log("diff: " + totalDiff);
+                // console.log(Math.min(...totalDiff));
+                lowDiff = Math.min(...totalDiff);
+                arrayPosition = totalDiff.indexOf(lowDiff)
+                // console.log(totalDiff.indexOf(lowDiff));
+                var match = friendsData[arrayPosition].name;
+                var picture = friendsData[arrayPosition].photo;
+
+                return {matchkey: match, picturekey: picture}
+                
+            }
+           
+    
 }
